6fbad72f09513b9e0ebdc689f5d1b15d1087e09c
Settpaing <htoosettpaing8@gmail.com>
2020-01-22 18:34:57 +0630

    blog end

---
+++ src/main/java/com/example/demosample/service/BlogService.java
@@ -0,0 +1,12 @@
+package com.example.demosample.service;
+
+import com.example.demosample.domain.Blog;
+
+import java.util.List;
+
+public interface BlogService {
+
+    Blog create(Blog blog);
+    Blog findById(int id);
+    List<Blog> findAll();
+}
